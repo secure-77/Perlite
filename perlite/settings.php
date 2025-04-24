baa74e037dcf09988c9031cd2a2e3f69b66e03f2
sec77 <31564517+secure-77@users.noreply.github.com>
2025-04-24 22:53:26 +0200

    include settings.php

---
+++ perlite/settings.php
@@ -0,0 +1,35 @@
+<?php
+$rootDir = "Demo";
+$vaultName = "Demo";
+$hideFolders = "docs,trash";
+$relPathes = False;
+$uriPath = "/";
+$siteTitle = "Perlite";
+$siteType = "article";
+$siteImage = "https://raw.githubusercontent.com/secure-77/Perlite/main/screenshots/screenshot.png";
+$siteURL = "https://perlite.secure77.de";
+$siteLogo = "perlite.svg";  // empty for no logo/customSection
+$siteDescription = "A web based markdown viewer optimized for Obsidian Notes";
+$siteName = "Perlite Demo";
+$siteHomepage = "";  // empty for $siteURL
+$siteGithub = "https://github.com/secure-77";  // empty for no Github
+$siteTwitter = "@secure_sec77";
+
+$tempPath = "";  // blanc so it gets it automatically
+
+$lineBreaks = true;
+
+$allowedFileLinkTypes = ['pdf', 'mp4'];
+
+$disablePopHovers = "false";
+$showTOC = "true";
+$showLocalGraph = "true";
+
+$index = "README";
+
+$font_size = "15";
+
+$htmlSafeMode = true;
+
+
+?>
